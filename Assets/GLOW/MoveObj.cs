a254446b4e9bae5413f2eacacfec248cbab2ab32
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2020-01-12 12:08:21 +0530

    Proper Collision and touch drag controls

--- Assets/GLOW/MoveObj.cs
+++ Assets/GLOW/MoveObj.cs
@@ -8,7 +8,8 @@ public class MoveObj : MonoBehaviour
     Vector3 mOffset;
     float mzcord;
     bool beingTouched = false;
-    //1440X720-18:9 Y(mouse/touch)->Z X(mouse/touch)->X
+    Vector3 originalPos;
+    //1440X720-18:9 Y(mouse/touch)->Z(Transform) X(mouse/touch)->X(Transform)
 
     private void OnMouseDown()
     {
@@ -26,21 +27,36 @@ public class MoveObj : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        //Vector3 moveposGlobal = GetmouseworldPos() + mOffset;
-        //transform.localPosition = new Vector3(moveposGlobal.x,1f,moveposGlobal.y);
-        transform.localPosition = GetmouseworldPos() + mOffset;
+        if (beingTouched)
+        {
+            Vector3 moveposGlobal = GetmouseworldPos() + mOffset;
+            transform.localPosition = new Vector3(moveposGlobal.x, 1f, moveposGlobal.y);
+            transform.localPosition = GetmouseworldPos() + mOffset;
+        }
     }
     private void OnMouseUp()
     {
         beingTouched = false;
+        Debug.Log("hre");
     }
     private void OnCollisionEnter(Collision collision)
     {
         
         if (collision.gameObject.tag=="Cube") {
-            Debug.Log("hre");
             if(!beingTouched)
-            gameObject.GetComponent<MeshRenderer>().material.color = collision.gameObject.GetComponent<Renderer>().material.color;
+                gameObject.GetComponent<MeshRenderer>().material.color = collision.gameObject.GetComponent<Renderer>().material.color;
+            beingTouched = false;
+        }
+    }
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+    private void Update()
+    {
+        if (!beingTouched)
+        {
+            transform.position = Vector3.Lerp(transform.position,originalPos,0.05f);
         }
     }
 }
